9afa6e5fd92e7d7c274750bac541fe0d8894baa1
john-josue-123 <114054412+john-josue-123@users.noreply.github.com>
2023-02-02 17:36:24 +0100

    Update osm.sh

--- osm.sh
+++ osm.sh
@@ -1,4 +1,4 @@
-osm repo-add --type helm-chart helmchartrepo https://Pabloj20.github.io/repo-rdsv
+osm repo-add --type helm-chart helmchartrepo https://john-josue-123.github.io/rdsv_final
 osm nfpkg-create ./shared/repo-rdsv/pck/accessknf_vnfd.tar.gz
 osm nfpkg-create ./shared/repo-rdsv/pck/cpeknf_vnfd.tar.gz
 osm nspkg-create ./shared/repo-rdsv/pck/renes_ns.tar.gz
