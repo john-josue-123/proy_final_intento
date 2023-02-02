8be138746f48234fb2b0c6c4f092a423a4421c86
john-josue-123 <114054412+john-josue-123@users.noreply.github.com>
2023-02-02 16:57:09 +0100

    Update arrancar_escenarios.sh

--- arrancar_escenarios.sh
+++ arrancar_escenarios.sh
@@ -3,14 +3,17 @@
 echo "MUIRST - RDSV - JOHN JOSUÉ CHÁVEZ ZEGARRA"
 echo "----------- Práctica final -----------------"
 
-echo " (1/3) - Ejecutando comandos ssh "
+echo " 1ero paso - Ejecutando comandos ssh en las maquinas para "
+echo " configurar la MTU de la interfaz eth1 en ambas máquinas. " 
 
 ssh upm@192.168.56.11 "sudo ip link set dev eth1 mtu 1400"
 ssh upm@192.168.56.12 "sudo ip link set dev eth1 mtu 1400"
 
 #Se arranca los escenarios de VNX
-echo " (2/3) - Arrancando VNX..."
+echo " 2do paso - Arrancando escenario VNX"
+
 ssh -l upm 192.168.56.11 "sudo vnx -f /home/upm/shared/rdsv_final/vnx/nfv3_home_lxc_ubuntu64.xml -t"
 ssh -l upm 192.168.56.11 "sudo vnx -f /home/upm/shared/rdsv_final/vnx/nfv3_server_lxc_ubuntu64.xml -t"
 ssh -l upm 192.168.56.11 "xhost +"
 
+echo "----------------------------"
