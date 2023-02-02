8a6e687fc3a614989296e21e61d9752d36e1552b
john-josue-123 <114054412+john-josue-123@users.noreply.github.com>
2023-02-02 17:47:10 +0100

    Update and rename qos.sh to QoS.sh

--- QoS.sh
+++ QoS.sh
@@ -11,10 +11,14 @@ set -u # to verify variables are defined
 : $HX2
 : $ACC_EXEC
 
-## Comandos CURL para QoS
-echo "## Comandos CURL para QoS"
+## Comandos para configurar QoS
+echo "## Comandos para configurar QoS"
 $ACC_EXEC curl -X PUT -d '"tcp:'${VNFTUNIP}':6632"' http://127.0.0.1:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
+echo "-----------------------------------------------------------------------------------------------------"
 $ACC_EXEC curl -X POST -d '{"port_name": "vxlanacc", "type": "linux-htb", "max_rate": "12000000", "queues": [{"min_rate": "8000000"}, {"max_rate": "4000000"}]}' http://127.0.0.1:8080/qos/queue/0000000000000001
+echo "-----------------------------------------------------------------------------------------------------"
 $ACC_EXEC curl -X POST -d '{"match": {"dl_dst": "'${HX1}'", "dl_type": "IPv4"}, "actions":{"queue": "0"}}' http://127.0.0.1:8080/qos/rules/0000000000000001
+echo "-----------------------------------------------------------------------------------------------------"
 $ACC_EXEC curl -X POST -d '{"match": {"dl_dst": "'${HX2}'", "dl_type": "IPv4"}, "actions":{"queue": "1"}}' http://127.0.0.1:8080/qos/rules/0000000000000001
-
+echo "-----------------------------------------------------------------------------------------------------"
+echo "QoS configurado"
